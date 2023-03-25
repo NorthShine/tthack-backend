3e5dd049f5fddb7e05dbf8eddc31de4b540521e5
bibiba <vyckluchi75@gmail.com>
2023-03-25 18:01:52 +0300

    fix streaming - finally

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+import os
 import base64
 import typing
-from pathlib import Path
 
 import cv2
 import numpy as np
-from fastapi import FastAPI, Header
-from fastapi.responses import Response
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 from fastapi.requests import Request
 
 from contrast import edit_contrast
@@ -13,38 +13,82 @@ from contrast import edit_contrast
 app = FastAPI(debug=True)
 
 
+def parse_range_request_header(str_header: str) -> dict:
+    header_content = {
+        "unit": "bytes",
+        "start_range": 0,
+        "end_range": None
+    }
+
+    range_header: list[str] = str_header.split("=")
+    if len(range_header) < 2:
+        return header_content
+
+    header_content["unit"] = range_header[0].strip()
+    range_header[1] = range_header[1].strip()
+    chunk_range: list[str] = range_header[1].split("-")
+    if len(chunk_range) == 2:
+        header_content["start_range"] = int(chunk_range[0])
+
+        if chunk_range[1].isnumeric():
+            header_content["end_range"] = int(chunk_range[1])
+
+    return header_content
+
+
 @app.get("/stream/{title}")
 async def stream_video(
         request: Request,
         title: str,
-        range: str = Header(None),
         contrast: typing.Optional[int | float] = None,
 ):
-    body = b''
-    path = Path(f"media/{title}")
-    with open(path, "rb") as video:
-        start, end = range.replace("bytes=", "").split("-")
-        start = int(start)
-        end = int(end) if end else start + 2048
-        video.seek(1)
-        data = video.read(end - start)
-        filesize = str(path.stat().st_size)
-        headers = {
-            'Content-Range': f'bytes {str(start)}-{str(end)}/{filesize}',
-            'Accept-Ranges': 'bytes',
-        }
+    file_path: str = f"media/{title}"
+    mimetype: str = "video/mp4"
+    file_stat = os.stat(file_path)
+    content_length: int = file_stat.st_size
 
-        """
-        for chunk in data:
-            if contrast:
-                frame = np.asarray(bytearray(chunk), np.uint8)
-                frame = cv2.imdecode(frame, -1)
-                frame = edit_contrast(frame, contrast)
-                frame = cv2.imencode('.jpg', frame)[1]
-                chunk = base64.b64encode(frame).decode('utf-8')
-            body += chunk
-        """
+    range_unit: str = "bytes"
+    start_range: int = 0
+    buffer_range: int = content_length
 
-    # response = Response(body, media_type="text/plain", headers=headers)
-        response = Response(data, media_type="text/plain", headers=headers)
-        return response
+    headers = {}
+
+    def read_file():
+        nonlocal file_path, start_range, buffer_range
+        with open(file_path, "rb", 2048) as file_ref:
+            file_ref.seek(start_range)
+            yield file_ref.read(buffer_range)
+
+    if "range" in request.headers:
+        range_request = parse_range_request_header(request.headers["range"])
+
+        start_range = int(range_request["start_range"])
+        if start_range > content_length:
+            start_range = content_length
+
+        if range_request["end_range"] is not None:
+            buffer_range = int(range_request["end_range"]) - start_range
+
+        left_bytes: int = content_length - start_range
+        if buffer_range > left_bytes:
+            buffer_range = left_bytes
+
+    headers["Content-Range"] = f"{range_unit} {start_range}-{buffer_range + start_range}/{content_length}"
+    headers["Content-Length"] = str(buffer_range)
+
+    response_status: int = 206
+    if buffer_range == 0:
+        response_status = 204
+
+    return StreamingResponse(read_file(), response_status, headers, mimetype)
+
+"""
+for chunk in data:
+    if contrast:
+        frame = np.asarray(bytearray(chunk), np.uint8)
+        frame = cv2.imdecode(frame, -1)
+        frame = edit_contrast(frame, contrast)
+        frame = cv2.imencode('.jpg', frame)[1]
+        chunk = base64.b64encode(frame).decode('utf-8')
+    body += str(chunk).encode("utf-8")
+"""
