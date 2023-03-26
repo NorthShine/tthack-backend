02be4cba0f901d7a42199a7e8e83c6d845c25a15
bibiba <vyckluchi75@gmail.com>
2023-03-26 13:35:45 +0300

    fix streaming - finally

--- main_backup.py
+++ main_backup.py
@@ -1,111 +1,88 @@
-import os
-import base64
-import typing
-
-import cv2
-import numpy as np
+from imutils.video import VideoStream
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
-from fastapi.requests import Request
+import threading
+import imutils
+import time
+import cv2
+import uvicorn
+from multiprocessing import Process, Queue
+import subprocess
+import numpy as np
 
-from contrast import edit_contrast
+HTTP_PORT = 6064
+lock = threading.Lock()
+app = FastAPI()
 
-app = FastAPI(debug=True)
+manager = None
+count_keep_alive = 0
+
+width = 1280
+height = 720
+
+url_rtsp = 'rtsp://admin:admin123@192.168.0.150/'
+file_path = 'media/1.mp4'
 
 
-def parse_range_request_header(str_header: str) -> dict:
-    header_content = {
-        "unit": "bytes",
-        "start_range": 0,
-        "end_range": None
-    }
+def start_stream(url_rtsp, manager):
+    global width
+    global height
 
-    range_header: list[str] = str_header.split("=")
-    if len(range_header) < 2:
-        return header_content
+    # vs = VideoStream(url_rtsp).start()
+    cap = cv2.VideoCapture(file_path)
+    while cap.isOpened():
+        # frame = vs.read()
+        # frame = imutils.resize(frame, width=680)
+        # output_frame = frame.copy()
 
-    header_content["unit"] = range_header[0].strip()
-    range_header[1] = range_header[1].strip()
-    chunk_range: list[str] = range_header[1].split("-")
-    if len(chunk_range) == 2:
-        header_content["start_range"] = int(chunk_range[0])
+        # if output_frame is None:
+        #     continue
+        ret, frame = cap.read()
+        frame = cv2.convertScaleAbs(frame, alpha=2.5)
+        try:
+            flag, encodedImage = cv2.imencode(".jpg", frame)
+        except:
+            break
+        if not flag:
+            continue
+        manager.put(encodedImage)
 
-        if chunk_range[1].isnumeric():
-            header_content["end_range"] = int(chunk_range[1])
 
-    return header_content
+def streamer():
+    try:
+        while manager:
+            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+                   bytearray(manager.get()) + b'\r\n')
+    except GeneratorExit:
+        print("cancelled")
+
+
+def manager_keep_alive(p):
+    global count_keep_alive
+    global manager
+    while count_keep_alive:
+        time.sleep(1)
+        print(count_keep_alive)
+        count_keep_alive -= 1
+    p.kill()
+    time.sleep(.5)
+    p.close()
+    manager.close()
+    manager = None
 
 
 @app.get("/stream/{title}")
-async def stream_video(
-        request: Request,
-        title: str,
-        contrast: typing.Optional[int | float] = None,
-):
-    file_path: str = f"media/{title}"
-    # mimetype: str = "video/mp4"
-    file_stat = os.stat(file_path)
-    content_length: int = file_stat.st_size
+async def video_feed():
+    return StreamingResponse(streamer(), media_type="multipart/x-mixed-replace;boundary=frame")
 
-    range_unit: str = "bytes"
-    start_range: int = 0
-    buffer_range: int = content_length
 
-    headers = {}
-
-    def read_file():
-        video = cv2.VideoCapture(file_path)
-        nonlocal start_range, buffer_range
-        with open(file_path, "rb", 2048) as file_ref:
-            ret, frame = video.read()
-
-            # encode the frame as a JPEG image in memory
-            _, img = cv2.imencode('.jpg', frame)
-
-            # convert the image to a bytes object
-            img_bytes = img.tobytes()
-
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + img_bytes + b'\r\n')
-            # cv2.imshow('frame',frame)
-            # file_ref.seek(start_range)
-            # info = file_ref.read(buffer_range)
-            # decoded = cv2.imdecode(np.frombuffer(info, np.uint8), -1)
-            # cv2.normalize(decoded, decoded, 0, 255, cv2.NORM_MINMAX)
-            # encoded = cv2.imencode('.jpg', decoded)
-            # yield encoded.tobytes()
-
-    if "range" in request.headers:
-        range_request = parse_range_request_header(request.headers["range"])
-
-        start_range = int(range_request["start_range"])
-        if start_range > content_length:
-            start_range = content_length
-
-        if range_request["end_range"] is not None:
-            buffer_range = int(range_request["end_range"]) - start_range
-
-        left_bytes: int = content_length - start_range
-        if buffer_range > left_bytes:
-            buffer_range = left_bytes
-
-    headers["Content-Range"] = f"{range_unit} {start_range}-{buffer_range + start_range}/{content_length}"
-    headers['Content-Type'] = "multipart/x-mixed-replace;boundary=frame"
-    # headers["Content-Length"] = "81789"
-
-    response_status: int = 206
-    if buffer_range == 0:
-        response_status = 204
-
-    return StreamingResponse(read_file(), response_status, headers)
-
-"""
-for chunk in data:
-    if contrast:
-        frame = np.asarray(bytearray(chunk), np.uint8)
-        frame = cv2.imdecode(frame, -1)
-        frame = edit_contrast(frame, contrast)
-        frame = cv2.imencode('.jpg', frame)[1]
-        chunk = base64.b64encode(frame).decode('utf-8')
-    body += str(chunk).encode("utf-8")
-"""
+@app.get("/keep-alive")
+def keep_alive():
+    global manager
+    global count_keep_alive
+    count_keep_alive = 100
+    if not manager:
+        manager = Queue()
+        p = Process(target=start_stream, args=(url_rtsp, manager,))
+        p.start()
+        threading.Thread(target=manager_keep_alive, args=(p,)).start()
