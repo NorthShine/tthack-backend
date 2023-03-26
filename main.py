c6751cc70675ad617673316ad98103852f17ea62
scripthound <grinkiko@gmail.com>
2023-03-26 18:58:22 +0300

    learned how to fix video codec

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import typing
 import uuid
 import os
+from pathlib import Path
 
 import cv2
 import uvicorn
@@ -11,7 +12,7 @@ from vidgear.gears import CamGear, StreamGear, VideoGear, WriteGear
 
 app = FastAPI()
 stream = VideoGear(source="media/1.mp4").start()
-output_params = {"-fourcc": "mp4v", "-fps": 30, "-vcodec": "h264_vaapi"}
+output_params = {"-fourcc": "mp4v", "-fps": 30}
 queue = []
 
 
@@ -19,43 +20,29 @@ def video_generator():
     frame_rate = 60
     while True:
         frame = stream.read()
+        frame_shape = frame.shape
+        frame_width = frame_shape[1]
+        frame_height = frame_shape[0]
         if frame is None:
             break
 
         random_filename = uuid.uuid4()
-        writer = WriteGear(f"media/{random_filename}.mp4", compression_mode=False, **output_params)
+        # writer = WriteGear(f"media/{random_filename}.mp4", compression_mode=False, **output_params)
+        writer = cv2.VideoWriter(
+            f"media/{random_filename}.mp4",
+            cv2.VideoWriter_fourcc(*'mp4v'),
+            30,
+            (frame_width, frame_height)
+        )
         print(random_filename)
         queue.append(random_filename)        
         for _ in range(frame_rate * 2):
             writer.write(frame)
             frame = stream.read()
-        writer.close()
+        writer.release()
         yield random_filename
 
 generator = video_generator()
-
-
-def parse_range_request_header(str_header: str) -> dict:
-    header_content = {
-        "unit": "bytes",
-        "start_range": 0,
-        "end_range": None
-    }
-
-    range_header: list[str] = str_header.split("=")
-    if len(range_header) < 2:
-        return header_content
-
-    header_content["unit"] = range_header[0].strip()
-    range_header[1] = range_header[1].strip()
-    chunk_range: list[str] = range_header[1].split("-")
-    if len(chunk_range) == 2:
-        header_content["start_range"] = int(chunk_range[0])
-
-        if chunk_range[1].isnumeric():
-            header_content["end_range"] = int(chunk_range[1])
-
-    return header_content
 
 
 @app.get('/video_part/{video_filename}/')
