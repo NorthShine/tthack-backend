38e4a1a0168116b0291b0337a86b72348261c506
scripthound <grinkiko@gmail.com>
2023-03-26 16:25:24 +0300

    added two endpoints to start stream and follow the stream

--- main.py
+++ main.py
@@ -9,44 +9,49 @@ from fastapi.responses import StreamingResponse
 from vidgear.gears import CamGear, StreamGear, VideoGear, WriteGear
 
 app = FastAPI()
+stream = VideoGear(source="media/1.mp4").start()
+queue = []
 
 
-@app.get('/stream/{title}')
-def video_feed(title: str, alpha: float = 1):
+def video_generator():
     frame_rate = 60
-    video_size = (1920, 1080)
+    while True:
+        frame = stream.read()
+        if frame is None:
+            break
 
-    stream = VideoGear(source="media/1.mp4").start()
-    writer = WriteGear(f"{uuid.uuid4()}.mp4", compression_mode=False)
-
-    def video_generator():
-        while True:
-            frame = stream.read()
-            if frame is None:
-                break
-            for _ in range(frame_rate * 2):
-                writer.write(frame)
+        random_filename = uuid.uuid4()
+        writer = WriteGear(f"media/{random_filename}.mp4", compression_mode=False)
+        print(random_filename)
+        queue.append(random_filename)        
+        for _ in range(frame_rate * 2):
             writer.write(frame)
-            yield
+        writer.close()
+        yield random_filename
 
-    # def frame_generator():
-    #     while True:
-    #         frame = stream.read()
-    #         if frame is None:
-    #             break
-    #         ret, jpeg = cv2.imencode('.jpg', frame)
-    #         jpeg = jpeg.tobytes()
-    #         # yield the frame as an HTTP response so it can be sent over the web
-    #         yield (b'--frame\r\n'
-    #                b'Content-Type: video/mp4\r\n\r\n' + jpeg + b'\r\n')
+generator = video_generator()
 
-    # define the headers for the HTTP response
+
+@app.get('/video_part/{video_filename}/')
+async def get_video_filename(video_filename: str):
+    try:
+        next_filename = next(generator)
+    except StopIteration:
+        return {"detail": "stream_finished"}
+
+    def iterfile():
+        with open(f"media/{video_filename}", mode="rb") as file_like:
+            yield from file_like
     headers = {
-        'Content-Type': 'multipart/x-mixed-replace; boundary=frame'
+        "Next-Part": str(next_filename)
     }
+    return StreamingResponse(iterfile(), media_type="video/mp4", headers=headers)
 
-    # return a StreamingResponse object with the frame generator function and headers
-    return StreamingResponse(frame_generator(), headers=headers)
+
+@app.get('/start_streaming/')
+async def video_feed():
+    current_filename = next(generator)
+    return {'start_filename': current_filename}
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="127.0.0.1", port=5000, access_log=False)
