2e977568c96cacb8c4f851cef8e937f8cc1647b2
bibiba <vyckluchi75@gmail.com>
2023-03-28 01:10:08 +0300

    add contrast

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-import time
+import subprocess
 import typing
 import uuid
-import os
-from pathlib import Path
 
 import cv2
 import uvicorn
 from fastapi import FastAPI, Request
-from fastapi.responses import StreamingResponse, FileResponse
+from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-from vidgear.gears import CamGear, StreamGear, VideoGear, WriteGear
+from vidgear.gears import VideoGear
 
 app = FastAPI()
-stream = VideoGear(source="media/1.mp4").start()
 output_params = {"-fourcc": "mp4v", "-fps": 30}
 queue = []
 
@@ -26,33 +23,38 @@ app.add_middleware(
 )
 
 
-def video_generator():
+def video_generator(title, alpha):
+    stream = VideoGear(source=f"media/{title}").start()
     frame_rate = 60
     while True:
         frame = stream.read()
+        if frame is None:
+            break
         frame_shape = frame.shape
         frame_width = frame_shape[1]
         frame_height = frame_shape[0]
-        if frame is None:
-            break
 
-        random_filename = uuid.uuid4()
-        # writer = WriteGear(f"media/{random_filename}.mp4", compression_mode=False, **output_params)
+        random_filename = str(uuid.uuid4()) + ".mp4"
         writer = cv2.VideoWriter(
-            f"media/{random_filename}.mp4",
+            f"media/{random_filename}",
             cv2.VideoWriter_fourcc(*'mp4v'),
             30,
             (frame_width, frame_height)
         )
-        print(random_filename)
         queue.append(random_filename)        
         for _ in range(frame_rate * 2):
             writer.write(frame)
             frame = stream.read()
         writer.release()
-        yield random_filename
+        output = str(uuid.uuid4()) + ".mp4"
 
-generator = video_generator()
+        ffmpeg_str = f"ffmpeg -i media/{random_filename} -c:v libx264 -crf 20 -c:a copy media/{output}"
+        if alpha:
+            ffmpeg_str = f"ffmpeg -i media/{random_filename} -vf eq=contrast={alpha} -c:v libx264 -crf 20 -c:a copy media/{output}"
+
+        subprocess.run(ffmpeg_str.split())
+        subprocess.run(f"rm media/{random_filename}".split())
+        yield output
 
 
 @app.get('/video_part/{video_filename}/')
@@ -61,9 +63,9 @@ async def get_video_filename(request: Request, video_filename: str):
     return FileResponse(video_filename, media_type="video/mp4")
 
 
-@app.get('/start_streaming/')
-async def video_feed():
-    current_filename = next(generator)
+@app.get('/start_streaming/{title}')
+async def video_feed(title: str, alpha: typing.Optional[float] = None):
+    current_filename = next(video_generator(title, alpha))
     return {'start_filename': current_filename}
 
 
